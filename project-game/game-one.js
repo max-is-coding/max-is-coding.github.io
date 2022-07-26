f9f61ba9bb78b4eadcdaf1a503021d4f911dd6bc
Amber Hilton <theloyalbadger@gmail.com>
2022-07-26 16:23:06 +1200

    added scrolling background

--- project-game/game-one.js
+++ project-game/game-one.js
@@ -2,24 +2,18 @@ const startBtn = document.getElementById("start-button")
 const title = document.getElementById("game-title")
 const titleRoom = document.getElementById("title-room")
 const barfGif = document.getElementById("barf-gif")
-const gameScreen = document.getElementById("game-area")
+const gameScreen = document.getElementsByClassName("game-area")[0]
 const mrBarf = document.getElementById("barf")
 let barfImg = document.createElement("img")
 barfImg.src = "./game-images/creature-2.png"
 barfImg.id = "jumpBarf"
 
-let keys = {
-    right: false,
-    left: false,
-    up: false
-}
+let bgImg = document.createElement("img")
+bgImg.src = "./game-images/total-bg.png"
+bgImg.id = "background"
 
-let player = {
-    x: 100,
-    y: 100,
-    width: 72,
-    height: 72
-}
+let runWav = new Audio('./sounds/running.wav')
+let landWav = new Audio('./sounds/landing.wav')
 
 startBtn.addEventListener("click", startGame)
 
@@ -33,11 +27,13 @@ function startGame() {
     $("#game-title").fadeOut("slow");
     barfGif.src = "./game-images/creature-2.png"
     titleBarfFalls()
-    console.log("game started")
 }
+
+
 
 function barfAppears () {
     barf.append(barfImg)
+    setTimeout(landSound, 50)
     $("#barf").delay(90).show( function(){
         $("#jumpBarf").attr("src", "./game-images/creature-1.png")
         setTimeout(barfRunAni, 700)
@@ -47,14 +43,39 @@ function barfAppears () {
 function barfRunAni() {
     $("#jumpBarf").attr("src", "./game-images/creature-run.gif")
     $("#barf").animate({
-        left: 1500
-    }, 3000, 'swing')
+        left: 900
+    }, 1500, 'swing')
+    setTimeout(runSound, 50)
     setTimeout(byeTitle, 1500)
+    setTimeout(newGame, 2000)
 }
 
 function byeTitle() {
-    $("#barf").fadeOut("3000")
-    $("#title-room").fadeOut("3000")
+    $("#title-room").fadeOut("5000", function(){
+        runWav.pause()
+        runWav.currentTime = 0
+    })
+}
+
+function newGame() {
+    mrBarf.style.left = "-50px"
+    mrBarf.style.bottom = "100px"
+    $("#barf").animate({
+        left: "100px"
+    }, 200, 'linear')
+    gameScreen.append(bgImg)
+    bgImg.appendChild(bgImg);
+}
+
+function landSound() {
+    landWav.currentTime = 0
+    landWav.play()
+}
+
+function runSound() {
+    runWav.currentTime = 0
+    runWav.play()
+    runWav.loop=true
 }
 
 function titleBarfFalls () {
@@ -73,18 +94,12 @@ function titleBarfFalls () {
     })
 }
 
-document.addEventListener(`keydown`, function(e) {
+document.addEventListener("keydown", function(e) {
     switch (e.key) {
 
-      case  `KeyW`:
+      case  "KeyW":
         keys.up = true;
         break;
-      case  `KeyD`:
-        keys.right = true;
-        break;
-      case  `KeyA`:
-        keys.left = true;
-        break; 
     }
 })
 
@@ -94,21 +109,5 @@ document.addEventListener("keyup", function(e){
         case  `KeyW`:
           keys.up = false;
           break;
-        case  `KeyD`:
-          keys.right = false;
-          break;
-        case  `KeyA`:
-          keys.left = false;
-          break; 
     }
 })
-
-function input() {
-    if(keys.right) {
-        player.x += 3
-    } else if (keys.left) {
-        player.x -= 3
-    } else if (keys.up) {
-        player.y -= 3
-    }
-}
